010ecc8a89a77d276e5d2e15fbfc13cbaf4e6634
tsukasa10032 <stynoyouxiang@qq.com>
2025-11-25 20:19:12 +0800

    Add files via upload
    day1.尝试添加了第一个类，圆形类。

---
+++ src/pysical_manager.cpp
@@ -0,0 +1,13 @@
+template <typename T,typename U>
+class pysical_manager
+{
+    private:
+        T object1;
+        U object2;
+
+    public:
+        pysical_manager(T one,U two):object1(),object2{}
+
+        
+
+}
