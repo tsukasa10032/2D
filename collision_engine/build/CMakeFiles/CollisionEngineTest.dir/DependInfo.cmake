e9aa4f63ba92728c857d105401c34e088b9c4114
tsukasa10032 <stynoyouxiang@qq.com>
2025-12-10 11:38:25 +0800

    进行了编译和测试

---
+++ collision_engine/build/CMakeFiles/CollisionEngineTest.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/main_test/function_in_main/add_a_circle.cpp" "CMakeFiles/CollisionEngineTest.dir/main_test/function_in_main/add_a_circle.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/main_test/function_in_main/add_a_circle.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/main_test/function_in_main/add_a_polygon.cpp" "CMakeFiles/CollisionEngineTest.dir/main_test/function_in_main/add_a_polygon.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/main_test/function_in_main/add_a_polygon.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/main_test/function_in_main/graphic_create.cpp" "CMakeFiles/CollisionEngineTest.dir/main_test/function_in_main/graphic_create.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/main_test/function_in_main/graphic_create.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/main_test/main.cpp" "CMakeFiles/CollisionEngineTest.dir/main_test/main.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/main_test/main.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_response/circle_circle_collision.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_response/circle_circle_collision.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_response/circle_circle_collision.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_response/circle_polygon_collision.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_response/circle_polygon_collision.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_response/circle_polygon_collision.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_response/polygon_polygon_collision.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_response/polygon_polygon_collision.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_response/polygon_polygon_collision.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_visitor/get_normals.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/get_normals.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/get_normals.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_visitor/is_circle_circle_collision.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/is_circle_circle_collision.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/is_circle_circle_collision.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_visitor/is_polygon_circle_collision.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/is_polygon_circle_collision.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/is_polygon_circle_collision.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_visitor/is_polygon_polygon_collision.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/is_polygon_polygon_collision.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/is_polygon_polygon_collision.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_visitor/projection.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/projection.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_visitor/projection.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/collision_world/collision_edge_detect.cpp" "CMakeFiles/CollisionEngineTest.dir/src/collision_world/collision_edge_detect.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/collision_world/collision_edge_detect.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/polygon_object/cal_axis_type.cpp" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/cal_axis_type.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/cal_axis_type.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/polygon_object/cal_centroid.cpp" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/cal_centroid.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/cal_centroid.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/polygon_object/cal_moment_of_inertia.cpp" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/cal_moment_of_inertia.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/cal_moment_of_inertia.cpp.obj.d"
+  "C:/Users/styno/Desktop/yin yue/2D/collision_engine/src/polygon_object/update.cpp" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/update.cpp.obj" "gcc" "CMakeFiles/CollisionEngineTest.dir/src/polygon_object/update.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ collision_engine/build/CMakeFiles/CollisionEngineTest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CollisionEngineTest.
+# This may be replaced when dependencies are built.
